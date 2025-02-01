cc8674bb0fa8ced14be959392139cda6ce9725c4
Varsha S <96822806+VarshaS24@users.noreply.github.com>
2025-02-01 12:47:23 +0530

    Add files via upload

---
+++ 1/script.js
@@ -0,0 +1,8 @@
+
+
+var typed=new Typed(".typing",{
+    strings:["Web Developer","Front-end Developer","Back-end Developer","Video Editor"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
